22eb3fcaca8607ade6bcbf04e05fa3d6b18e5c30
victoriamnx <victoriamarques0604@gmail.com>
2023-02-06 23:02:41 -0300

    Adicionado dados.js

--- js/impar_par.js
+++ js/impar_par.js
@@ -1,17 +1,16 @@
-let impar = 'Números Impares:'
 let par = 'Números Pares:'
+let impar = 'Números Impares:'
 
-
-for (let value = 0; value <= 100; value++) {
+for (let value = 0; value <= 10; value++) {
     
     if (value %2 === 0) {
         par += "\n" + value
     }
-
+   
     else {
         impar += "\n" + value
     }
 } 
 
-console.log(par)
 console.log(impar)
+console.log(par)
