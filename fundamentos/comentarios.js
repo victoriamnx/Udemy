b0434201c1c908180a92874d54254b3115db413e
victoriamnx <victoriamarques0604@gmail.com>
2023-02-21 18:46:00 -0300

    add: pasta fundamentos

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,14 @@
+// Comentários de uma linha 
+console.log('Linha 1')
+
+/*
+Comentários de
+múltipla linhas
+*/
+console.log('Linha 2')
+
+/*
+* Comentários de
+* múltipla linhas
+*/
+console.log('Linha 3')
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,6 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
