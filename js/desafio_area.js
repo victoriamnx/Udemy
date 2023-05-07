2e43b6a39480d4494db747eb10b8432021b4304e
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-05-07 20:47:22 -0300

    fix: organization

--- js/desafio_area.js
+++
@@ -1,9 +0,0 @@
-const PI = 3.141592;
-let raio = 10; 
-let areaCirc = PI * raio * raio;
-
-console.log("O valor da área é " + areaCirc + "m2.")
-
-
-areaCirc = Math.PI * raio * raio;
-console.log("O valor da área é " + areaCirc + "m2.")
