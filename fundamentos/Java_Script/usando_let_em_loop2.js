840a4d05e66b884d452d2eadf137a64009555f2f
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-05-13 08:51:43 -0300

    add: new js files

--- fundamentos/Java_Script/usando_let_em_loop2.js
+++ fundamentos/Java_Script/usando_let_em_loop2.js
@@ -1,11 +1,11 @@
-var funcs = []
+var funcs = [];
 
 for (let i = 0; i < 10; i++) {
-    funcs.push(function() {
-        console.log(i)
-    })
+  funcs.push(function () {
+    console.log(i);
+  });
 }
 
-funcs[2]()
-funcs[6]()
-funcs[8]()
+funcs[2]();
+funcs[6]();
+funcs[8]();
--- fundamentos/Java_Script/usando_var_em_loop copy 2.js
+++ fundamentos/Java_Script/usando_var_em_loop copy 2.js
@@ -1,5 +1,5 @@
-for ( var i = 0; i < 10; i++) {
-    console.log(i)
+for (var i = 0; i < 10; i++) {
+  console.log(i);
 }
 
-console.log("i =", i)
+console.log("i =", i);
